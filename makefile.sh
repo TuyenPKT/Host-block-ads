b2b63ed5a9e069571c9317058d025be356c61a98
Tuyên'PKT <8386909+TuyenPKT@users.noreply.github.com>
2024-05-14 23:53:49 +0700

    Update makefile.sh

--- makefile.sh
+++ makefile.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+echo "Making titles..."
+# make time stamp & count blocked
+TIME_STAMP=$(date +'%d %b %Y %H:%M')
+VERSION=$(date +'%y%m%d%H%M')
+LC_NUMERIC="en_US.UTF-8"
+
 # update titles
-sed -e "s/_time_stamp_/$TIME_STAMP/g" -e "s/_version_/$VERSION/g" -e "s/_domain_/$DOMAIN/g" tmp/title-Black.txt > tmp/title-Black.tmp
-sed -e "s/_time_stamp_/$TIME_STAMP/g" -e "s/_version_/$VERSION/g" -e "s/_domain_/$DOMAIN/g" tmp/title-White.txt > tmp/title-White.tmp
+sed -e "s/_time_stamp_/$TIME_STAMP/g" -e "s/_version_/$VERSION/g" tmp/title-Black.txt > tmp/title-Black.tmp
+sed -e "s/_time_stamp_/$TIME_STAMP/g" -e "s/_version_/$VERSION/g" tmp/title-White.txt > tmp/title-White.tmp
 
 echo "Creating hosts file..."
 # create hosts files
