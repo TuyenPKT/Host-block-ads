b6d2a70319bcd84cb7a43b9264394eced575aab6
Tuyên'PKT <8386909+TuyenPKT@users.noreply.github.com>
2024-05-15 17:47:03 +0700

    Update makefile.sh

--- makefile.sh
+++ makefile.sh
@@ -4,7 +4,7 @@ echo "Making titles..."
 # make time stamp & count blocked
 TIME_STAMP=$(date +'%d %b %Y %H:%M')
 VERSION=$(date +'%y%m%d%H%M')
-LC_NUMERIC="en_US.UTF-8"
+
 
 # update titles
 sed -e "s/_time_stamp_/$TIME_STAMP/g" -e "s/_version_/$VERSION/g" tmp/title-Black.txt > tmp/title-Black.tmp
@@ -17,10 +17,6 @@ cat tmp/title-Black.tmp source/Black-list.txt source/White-list.txt > hosts
 # remove tmp file
 rm -rf tmp/*.tmp
 
-# check duplicate
-echo "Checking duplicate..."
-sort source/White-list.txt | uniq -d
-sort source/Black-list.txt | uniq -d
 
 
 
