2c7a120fe12ea7b0cc5b2b053254e613a9f0a5a5
Tuyên'PKT <8386909+TuyenPKT@users.noreply.github.com>
2024-05-16 18:52:11 +0700

    Update host.sh

--- host.sh
+++ host.sh
@@ -1,10 +1,17 @@
+#!/bin/sh
 
+echo "Making titles..."
+# make time stamp & count blocked
+TIME_STAMP=$(date +'%d %b %Y %H:%M')
+VERSION=$(date +'%y%m%d%H%M')
 
+echo "Making titles..."
+sed -e "s/_time_stamp_/$TIME_STAMP/g" -e "s/_version_/$VERSION/g" tmp/title-Black.txt > tmp/title-Black.tmp
+sed -e "s/_time_stamp_/$TIME_STAMP/g" -e "s/_version_/$VERSION/g" tmp/title-White.txt > tmp/title-White.tmp
 
-
-
-
-
+echo "Creating hosts file..."
+cat tmp/title-Black.tmp source/Black-list.txt > Black
+cat tmp/title-White.tmp source/White-list.txt > White
 
 echo "Checking duplicate..."
 sort source/Black-list.txt | uniq -d
