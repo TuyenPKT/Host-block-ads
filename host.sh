2a0381af7d401d0bf87980bc006b010810f07339
Tuyên'PKT <8386909+TuyenPKT@users.noreply.github.com>
2024-05-17 16:09:25 +0700

    Update host.sh

--- host.sh
+++ host.sh
@@ -5,8 +5,8 @@ echo "Making titles..."
 TIME_STAMP=$(date +'%d %b %Y %H:%M')
 VERSION=$(date +'%y%m%d%H%M')
 
-sed -e "s/_time_stamp_/$TIME_STAMP/g" -e "s/_version_/$VERSION/g" source/Black-list.txt > source/Black-list.txt
-sed -e "s/_time_stamp_/$TIME_STAMP/g" -e "s/_version_/$VERSION/g" source/Black-list.txt > source/Black-list.txt
+sed -e "s/_time_stamp_/$TIME_STAMP/g" -e "s/_version_/$VERSION/g" source/Black-list.txt > Black-hosts
+sed -e "s/_time_stamp_/$TIME_STAMP/g" -e "s/_version_/$VERSION/g" source/White-list.txt > White-hosts
 
 echo "Checking duplicate..."
 sort source/Black-list.txt | uniq -d
