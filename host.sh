f5e0a7c1ee0e3fb6e28f9efccf169842f67e1c83
Tuyên'PKT <8386909+TuyenPKT@users.noreply.github.com>
2024-05-17 16:07:45 +0700

    Update host.sh

--- host.sh
+++ host.sh
@@ -5,7 +5,6 @@ echo "Making titles..."
 TIME_STAMP=$(date +'%d %b %Y %H:%M')
 VERSION=$(date +'%y%m%d%H%M')
 
-
 sed -e "s/_time_stamp_/$TIME_STAMP/g" -e "s/_version_/$VERSION/g" source/Black-list.txt > source/Black-list.txt
 sed -e "s/_time_stamp_/$TIME_STAMP/g" -e "s/_version_/$VERSION/g" source/Black-list.txt > source/Black-list.txt
 
