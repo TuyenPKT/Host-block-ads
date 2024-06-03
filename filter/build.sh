f413ab9650145a6d7af99b2dd96d6c162da260b9
TuyenPKT <admin@tuyen.vn>
2024-06-03 17:05:32 +0700

    first commit

--- filter/build.sh
+++ filter/build.sh
@@ -5,15 +5,9 @@ VERSION=$1
 if [[ -z $VERSION ]]; then
     VERSION=`date +'%Y%m%d%H%M%S%3N'`
 fi
-
 sed -e "s/_time_stamp_/$TIME_STAMP/g" -e "s/_version_/$VERSION/g" src/Black_title.txt > src/Black_title.tmp
-
-
-
 cat src/Black_title.tmp src/Black_list.txt > Black_list.tmp
 cat Black_list.tmp src/White_list.txt > Hosts
-
-
 rm -rf *.tmp src/*.tmp
 
 
--- filter/git.sh
+++ filter/git.sh
@@ -3,4 +3,4 @@ git config --global user.email admin@tuyen.vn
 git add .
 git status	
 git commit -m "first commit"
-git push
+git push -f
