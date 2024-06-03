3b7cce0024e0cf25f6ff68a650b5fa4a864ad112
TuyenPKT <admin@tuyen.vn>
2024-06-03 17:22:06 +0700

    first commit

--- filter/build.sh
+++ filter/build.sh
@@ -7,7 +7,7 @@ if [[ -z $VERSION ]]; then
 fi
 sed -e "s/_time_stamp_/$TIME_STAMP/g" -e "s/_version_/$VERSION/g" src/Black_title.txt > src/Black_title.tmp
 cat src/Black_title.tmp src/Black_list.txt > Black_list.tmp
-cat Black_list.tmp src/White_list.txt > Hosts
+cat Black_list.tmp src/White_list.txt > ../Hosts
 rm -rf *.tmp src/*.tmp
 
 
